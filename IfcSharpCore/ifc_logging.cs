b8d72517f70d11feb04f59939035a472ed5d0156
friedrichEder <friedrich_eder@outlook.de>
2022-07-03 20:20:55 +0200

    added simple filter for logging

--- IfcSharpCore/ifc_logging.cs
+++ IfcSharpCore/ifc_logging.cs
@@ -1,9 +1,9 @@
 using NetSystem = System;
 using System.IO;
+using System.Linq;
 
 namespace ifc
 {
-    //TODO: implement filter for output
     public static class Log
     {
         public enum Level
@@ -15,25 +15,32 @@ namespace ifc
             Exception
         };
 
+        private static Level[] Filter = new[] {Level.Info};
         private const bool OutputToConsole = true;
         private static string logFilePath = "ifcsharp.log";
         public static void Add(string msg, Level level) {
             string log = $"{NetSystem.DateTime.Now:O}[{level}]: {msg}";
             if(OutputToConsole) NetSystem.Console.WriteLine(log);
-            try {
-                using (StreamWriter sw = new StreamWriter(path: logFilePath, append: true)) {
-                    sw.WriteLine(log);
-                    sw.Close();
+            if (false == Filter.Contains(level)) {
+                try {
+                    using (StreamWriter sw = new StreamWriter(path: logFilePath, append: true)) {
+                        sw.WriteLine(log);
+                        sw.Close();
+                    }
+
                 }
-            }
-            catch(NetSystem.Exception e)
-            {
-                NetSystem.Console.WriteLine(e);
+                catch (NetSystem.Exception e) {
+                    NetSystem.Console.WriteLine(e);
+                }
             }
         }
 
         public static void SetLogfilePath(string path) {
             logFilePath = path;
+        }
+
+        public static void SetFilter(Level[] filter) {
+            Filter = filter;
         }
         public static void Reset() {
             if(File.Exists(logFilePath)) File.Delete(logFilePath);
